c5721dd51e23843dd9a39d0ff5ccd60f6ef5e241
Jérémy <jeremy.hennebique@easiware.fr>
2023-04-05 16:40:04 +0200

    amélioration du serveur

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,23 @@
 const express = require('express');
 
 const app = express();
+app.use((req, res, next) => {
+     console.log("requete reçue");
+     next();
+ });
+
+ app.use((req,res, next) => {
+    res.status(201);
+    next();
+ });
+
+app.use((req, res, next) => {
+   res.json({ message: 'Votre requête a bien été reçue !' });
+   next(); 
+});
 
 app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
-});
+    console.log('réponse envoyée avec succès'); 
+ });
 
 module.exports = app;
