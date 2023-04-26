17d3215fc9c3daea94ab1bedd7d2b344442aec9a
Jérémy <jeremy.hennebique@easiware.fr>
2023-04-26 15:52:39 +0200

    update controller stuff

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,52 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const Thing = require('../Models/Thing');
+const stuffCtrl = require('..controllers/stuff');
+
 
 //poster un nouvel item
-router.post('/', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-      ...req.body
-    });
-    thing.save()
-    //save est une methode fournie par mongoose
-      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-  });
-  
-  //modifier 1 item
-  router.put('/:id', (req, res, next) => {
-    Thing.updateOne({_id: req.params.id},{...req.body, _id:req.params.id})
-    .then(thing => res.status(200).json({message :'Objet modifié'}))
-    .catch(error => res.status(400).json({error}));
-  
-  });
-  
-  //supprimer 1 item
-  router.delete('/:id',(req, res, next) => {
-    Thing.deleteOne({_id: req.params.id})
-    .then(thing => res.status(200).json({message:'Objet supprimé ! '}))
-    .catch(error => res.status(400).json({error}));
+router.post('/', stuffCtrl.createThing);
     
-  })
   
-  //Récupérer 1 item
-  router.get('/:id', (req, res, next) => {
-    //req.params.id est l'id passé dynamiquement
-    //findOne est une methode fournie par mongoose
-    Thing.findOne({_id: req.params.id})
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(404).json({error}));
-    
-  });
+//modifier 1 item
+router.put('/:id', stuffCtrl.modifyThing);
   
-  //récupérer tous les items
-  router.get('/', (req, res, next) => {
-    //find est une méthode fournie par mongoose
-      Thing.find()
-      .then(things => res.status(200).json(things))
-      .catch(error => res.status(400).json({error}));
-    });
+//supprimer 1 item
+router.delete('/:id',stuffCtrl.deleteThing);
+  
+//Récupérer 1 item
+router.get('/:id', stuffCtrl.retrieveItem);
+  
+//récupérer tous les items
+  router.get('/', stuffCtrl.findItems);
 
 
 
